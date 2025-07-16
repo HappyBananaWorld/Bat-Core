c7abb9bab8ab821be94bb85575918059e545d74a
Mr Banana <sajad.dev.personal@gmail.com>
2025-07-16 23:10:37 +0330

    testing render view & setup server to watch any changes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
+use Nyholm\Psr7\Response;
 
 function registerRoutes($app)
 {
     $app->get('/', function () {
-        return "hi";
+        ob_start();
+        include __DIR__ . "/../Views/index.php";
+        $content = ob_get_clean();
+
+        return new Response(200, ['Content-Type' => 'text/html'], $content);
     });
 }
