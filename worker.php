d32aaa5841197276ffa78604b702ef9a0ca03e32
Mr Banana <sajad.dev.personal@gmail.com>
2025-07-16 20:30:27 +0330

    create simple class for handling routes core

--- worker.php
+++ worker.php
@@ -8,34 +8,68 @@ use Nyholm\Psr7\Factory\Psr17Factory;
 use Spiral\RoadRunner\Worker;
 use Spiral\RoadRunner\Http\PSR7Worker;
 
-$worker = Worker::create();
-$factory = new Psr17Factory();
-$psr7 = new PSR7Worker($worker, $factory, $factory, $factory);
 
-while (true) {
-    try {
-        $request = $psr7->waitRequest();
-        if ($request === null) {
-            break;
-        }
-    } catch (\Throwable $e) {
-        $psr7->respond(new Response(400));
-        continue;
+class BatCore
+{
+    private PSR7Worker $psr7;
+    private array $routes = [];
+
+    public function __construct()
+    {
+        $worker = Worker::create();
+        $factory = new Psr17Factory();
+        $this->psr7 = new PSR7Worker($worker, $factory, $factory, $factory);
     }
 
-    try {
-        $path = $request->getUri()->getPath();
+    public function get(string $path, callable $handler)
+    {
+        $this->routes["GET"][$path] = $handler;
+    }
 
-        if ($path === '/') {
-            $response = new Response(200, [], 'hi index');
-        } elseif ($path === '/cat') {
-            $response = new Response(200, [], 'meow');
-        } else {
-            $response = new Response(404, [], 'not found');
+    public function run(): void
+    {
+        while (true) {
+            try {
+                $request = $this->psr7->waitRequest();
+                if ($request === null) {
+                    break;
+                }
+            } catch (\Throwable $e) {
+                $this->psr7->respond(new Response(400));
+                continue;
+            }
+
+            try {
+                $path = $request->getUri()->getPath();
+
+                if ($path === '/') {
+                    $response = new Response(200, [], 'hi index');
+                } elseif ($path === '/cat') {
+                    $response = new Response(200, [], 'meow');
+                } else {
+                    $response = new Response(404, [], 'not found');
+                }
+
+                $this->psr7->respond($response);
+            } catch (\Throwable $e) {
+                $this->psr7->respond(new Response(500, [], 'server error'));
+            }
         }
+    }
 
-        $psr7->respond($response);
-    } catch (\Throwable $e) {
-        $psr7->respond(new Response(500, [], 'server error'));
+    public function __destruct()
+    {
+        $this->run();
     }
 }
+
+$app = new BatCore();
+
+$app->get('/', function () {
+    return 'hi index';
+});
+
+$app->get('/cat', function () {
+    return 'meow';
+});
+
